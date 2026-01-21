e20d957b80b069f0f526c87a5101344ef77c937f
Daniel John Baynosa <danieljohnbyns@gmail.com>
2026-01-21 19:29:52 +0800

    refactor: update .gitignore to include sandbox directory and ensure .gitkeep is tracked; add sandbox and memory file creation logic in MCP configuration

--- src/config/mcp.ts
+++ src/config/mcp.ts
@@ -1,4 +1,16 @@
+import fs from 'fs';
 import path from 'path';
+
+// if sandbox folder doesn't exist, create it
+const sandboxPath = path.join(process.cwd(), 'sandbox');
+if (!fs.existsSync(sandboxPath))
+	fs.mkdirSync(sandboxPath);
+
+// if memory file doesn't exist, create it
+const memoryFilePath = path.join(process.cwd(), 'sandbox', 'memory.jsonl');
+if (!fs.existsSync(memoryFilePath))
+	fs.writeFileSync(memoryFilePath, JSON.stringify([], null, 2));
+
 const config = [
 	{
 		command: 'npx',
@@ -14,6 +26,23 @@ const config = [
 			'@modelcontextprotocol/server-filesystem',
 			path.join(process.cwd(), 'sandbox')
 		]
+	},
+	{
+		command: 'npx',
+		args: [
+			'-y',
+			'@modelcontextprotocol/server-sequential-thinking'
+		]
+	},
+	{
+		command: 'npx',
+		args: [
+			'-y',
+			'@modelcontextprotocol/server-memory'
+		],
+		env: {
+			MEMORY_FILE_PATH: path.join(process.cwd(), 'sandbox', 'memory.json')
+		}
 	}
 ];
 
