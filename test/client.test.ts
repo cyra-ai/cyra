b1b447fa45cebd69dc81850220abdab9ec3f77e8
Daniel John Baynosa <danieljohnbyns@gmail.com>
2026-01-21 18:11:09 +0800

    refactor: comment out volume visualization in microphone input stream

--- test/client.test.ts
+++ test/client.test.ts
@@ -82,8 +82,8 @@ micInputStream.on('data', (data: Buffer) => {
 		}
 	};
 	ws.send(JSON.stringify(payload));
-	const volume = data.reduce((acc, val) => acc + Math.abs(val), 0) / data.length;
-	process.stdout.write(`\r[${'='.repeat(Math.min(Math.floor(volume / 10), 20)).padEnd(20)}]`);
+	// const volume = data.reduce((acc, val) => acc + Math.abs(val), 0) / data.length;
+	// process.stdout.write(`\r[${'='.repeat(Math.min(Math.floor(volume / 10), 20)).padEnd(20)}]`);
 });
 
 micInputStream.on('error', (err: Error) => {
