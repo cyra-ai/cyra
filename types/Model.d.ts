311f4649b428454ccd45fd72314323361b911b0d
Daniel John Baynosa <danieljohnbyns@gmail.com>
2026-02-05 06:44:57 +0800

    feat: define Model type with parameters and limits for structured data handling

---
+++ types/Model.d.ts
@@ -0,0 +1,13 @@
+type Model = {
+	id: string;
+	name: string;
+	description: string;
+	parameters?: string; // 1b, 2b, 3b, 4b, 8b, 13b, 70b, etc.
+	limits: {
+		rpm: number; // requests per minute
+		tpm: number; // tokens per minute
+		rpd: number; // requests per day
+	};
+};
+
+export default Model;
