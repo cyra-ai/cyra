0f6ee3f931e6d72f4d862272fa57146e10fbcab6
Daniel John Baynosa <danieljohnbyns@gmail.com>
2026-02-05 09:17:58 +0800

    fix: make parameters field required in Model type definition

--- types/Model.d.ts
+++ types/Model.d.ts
@@ -2,12 +2,13 @@ type Model = {
 	id: string;
 	name: string;
 	description: string;
-	parameters?: string; // 1b, 2b, 3b, 4b, 8b, 13b, 70b, etc.
+	parameters: string; // 1b, 2b, 3b, 4b, 8b, 13b, 70b, etc.
 	limits: {
 		rpm: number; // requests per minute
 		tpm: number; // tokens per minute
 		rpd: number; // requests per day
 	};
+	contextWindow: number; // context window size in tokens
 };
 
 export default Model;
